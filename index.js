14373183aaf00bbc60347cbfd3f96a19dfba8ccc
Ricardo <ricardo.pdelcastillo@uclm.es>
2018-04-12 11:04:07 +0000

    change /about

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.options("/*", (req, res, next) => {
 });
 
 app.get('/about', function(req, res) {
-  res.redirect("https://pafmon.github.io/rcis-test/");
+  res.redirect("https://ricpdc.github.io/RCIS18_PetStore_RPC/");
 });
 
 // swaggerRouter configuration
